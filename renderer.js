f4b37ba91dcad16145fc3c7e5f1689d6209f1424
Joseph Sammarco <SESA466246@schneider-electric.com>
2017-07-11 11:53:33 -0500

    titleless

--- renderer.js
+++ renderer.js
@@ -1,3 +1,16 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const remote = require('electron').remote;
+
+function minimizeWin(){
+	var window = remote.getCurrentWindow();
+	window.minimize();  
+}
+
+function closeWin(){
+	var window = remote.getCurrentWindow();
+	window.close();
+	app.quit();
+}
