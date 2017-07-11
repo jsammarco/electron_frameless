88bedaeaa5a968558bec399b38c28ed77c07e303
Joseph Sammarco <SESA466246@schneider-electric.com>
2017-07-11 12:56:01 -0500

    fixes for drag and selecting text on menu bar. Added status bar also.

--- main.js
+++ main.js
@@ -13,7 +13,12 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    title: "Password Access Request Console"
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
